0bfab5643250efdf6196fa92a92f49d78a25396a
Ednario Andrade <ednariocoder@gmail.com>
2023-12-12 17:12:47 -0300

    first commit

---
+++ app/components/header/index.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+
+import NavItem from './nav-item'
+
+const NAV_ITEMS = [
+  {
+    label: 'Home',
+    href: '/',
+  },
+  {
+    label: 'Projetos',
+    href: '/projetos',
+  },
+]
+function Header() {
+  return (
+    <header className="absolute top-0 w-full z-10 h-24 flex items-center justify-center">
+      <div className="container flex items-center justify-between">
+        <Link href="/">
+          <Image width={58} height={49} src="images/logo.svg" alt={''} />
+        </Link>
+
+        <nav className="flex items-center gap-4 sm:gap-10">
+          {NAV_ITEMS.map((item) => (
+            <NavItem key={item.label} {...item} />
+          ))}
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ app/components/header/nav-item.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { cn } from '@/app/lib/utils'
+
+type NavItemProps = {
+  label: string
+  href: string
+}
+
+function NavItem({ label, href }: NavItemProps) {
+  const pathname = usePathname()
+
+  const isActive = pathname === href
+  return (
+    <Link
+      href={href}
+      className={cn(
+        'text-gray-400 flex items-center gap-2 font-medium font-mono',
+        isActive && 'text-gray-50',
+      )}
+    >
+      <span className="text-emerald-400">#</span>
+      {label}
+    </Link>
+  )
+}
+
+export default NavItem

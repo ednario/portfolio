996e8774bea0dfedc64a18eab7f53d58b085015f
Ednario Andrade <ednariocoder@gmail.com>
2023-12-19 14:53:35 -0300

    feat: ensure contact session

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import { ReactNode } from 'react'
 
 import Header from './components/header'
+import ContactForm from './components/contact-form'
 
 const inter = Inter({
   variable: '--font-inter',
@@ -21,6 +22,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body>
         <Header />
         {children}
+        <ContactForm />
       </body>
     </html>
   )
